13f8588d13f6bc2ae390a4996429631922c84635
dodytrifa <dody.trifa@gmail.com>
2022-09-18 18:20:47 +0700

    promise challenge

--- promise.js
+++ promise.js
@@ -1,7 +1,41 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = (emosi) =>{
+  return new Promise ((resolve, reject) => {
+    let angryCounter = 0
+    let calmCounter = 0
+    promiseTheaterIXX()
+      .then((result) => {
+        result.forEach((element) => {
+          if (element.hasil === emosi) {
+            calmCounter++
+          } else {
+            angryCounter++;
+          }
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      })
+    promiseTheaterVGC()
+      .then((result) => {
+        result.forEach((element) => {
+          if (element.hasil === emosi) {
+            calmCounter++
+          } else {
+            angryCounter++;
+          }
+          });
+          console.log(calmCounter);
+          console.log(angryCounter);
+      })
+      .catch((error) => {
+        reject(error);
+      })
+    })
+}
 
 module.exports = {
   promiseOutput,
